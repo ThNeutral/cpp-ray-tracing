a7575ce164ee9074585b81f594d12db592e7382b
Kiril Shiyan <kiril.shiyan@accedia.com>
2025-01-10 15:47:17 +0200

    Made ray reflection

--- src/color.h
+++ src/color.h
@@ -5,10 +5,18 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear) {
+    if (linear > 0) {
+        return std::sqrt(linear);
+    }
+
+    return 0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
-    auto r = pixel_color.x();
-    auto g = pixel_color.y();
-    auto b = pixel_color.z();
+    auto r = linear_to_gamma(pixel_color.x());
+    auto g = linear_to_gamma(pixel_color.y());
+    auto b = linear_to_gamma(pixel_color.z());
 
     static const interval intensity(0.000, 0.999);
 
